cdaa0a5f4bf06072a8e10e852c62584954980e1c
Jian Sun <sunjxan@gmail.com>
2024-11-14 17:27:30 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ nvcc -O2 -std=c++17 -Xcompiler -Wall cpu_1.cu -o a && ./a
 nvcc -O2 -std=c++17 -Xcompiler -Wall -DUSE_DP cpu_1.cu -o a && ./a
 nvcc -O2 -std=c++17 -arch=sm_86 -Xcompiler -Wall --expt-relaxed-constexpr cuda_1.cu -o a && ./a
 nvcc -O2 -std=c++17 -arch=sm_86 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_1.cu -o a && ./a
-nvcc -O2 -std=c++17 -arch=sm_86 -Xcompiler -Wall --expt-relaxed-constexpr cuda_1.cu -lcublas -o a && ./a
-nvcc -O2 -std=c++17 -arch=sm_86 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_1.cu -lcublas -o a && ./a
+nvcc -O2 -std=c++17 -arch=sm_86 -Xcompiler -Wall --expt-relaxed-constexpr cublas.cu -lcublas -o a && ./a
+nvcc -O2 -std=c++17 -arch=sm_86 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cublas.cu -lcublas -o a && ./a
