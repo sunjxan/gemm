d1f8e2b7e4f7ae00a2fe0adda2af9dcfe53b1ee7
sunjian <sunjxan@gmail.com>
2024-12-16 15:55:04 +0800

    cpu 3

--- cpu_3.cu
+++ cpu_3.cu
@@ -2,25 +2,31 @@
 
 void matmul(const real *A, const real *B, real *C)
 {
-    CHECK(cudaMemset(C, 0, MN_size));
-
     const real (*nA)[K] = reinterpret_cast<decltype(nA)>(A);
     const real (*nB)[N] = reinterpret_cast<decltype(nB)>(B);
     real (*nC)[N] = reinterpret_cast<decltype(nC)>(C);
 
-    size_t Mtile = 128, Ntile = 128;
-    for (size_t m = 0; m < M; m += Mtile) {
-        for (size_t n = 0; n < N; n += Ntile) {
-            for (size_t t = 0; t < K; ++t) {
-                for (size_t i = 0; i < Mtile; ++i) {
-                    for (size_t j = 0; j < Ntile; ++j) {
-                        size_t row = m + i, col = n + j;
-                        nC[row][col] += nA[row][t] * nB[t][col];
-                    }
-                }
-            }
+    real *B2;
+    CHECK(cudaMallocHost(&B2, KN_size));
+    real (*nB2)[K] = reinterpret_cast<decltype(nB2)>(B2);
+
+    for (size_t i = 0; i < K; ++i) {
+        for (size_t j = 0; j < N; ++j) {
+            nB2[j][i] = nB[i][j];
         }
     }
+
+    for (size_t i = 0; i < M; ++i) {
+        for (size_t j = 0; j < N; ++j) {
+            real sum = 0.0;
+            for (size_t t = 0; t < K; ++t) {
+                sum += nA[i][t] * nB2[j][t];
+            }
+            nC[i][j] = sum;
+        }
+    }
+
+    CHECK(cudaFreeHost(B2));
 }
 
 int main()
