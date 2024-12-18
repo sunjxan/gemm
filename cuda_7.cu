3ee02d551539f6c1c13fbc7bd5a25bbe4b85b3de
sunjian <sunjxan@gmail.com>
2024-12-18 13:05:25 +0800

    cuda 7

--- cuda_7.cu
+++ cuda_7.cu
@@ -1,13 +1,13 @@
 #include "common.hpp"
 
-// 让最后一个小迭代为下一轮预取数据，从全局内存转移数据到共享内存拆分成两步，使用寄存器中转
+// 让最后一个小迭代为下一轮预取数据
 
 // block_shape应能整除M、K、N，block_unit应能整除K
 constexpr size_t block_shape = 128, block_unit = 8;
 // thread_shape应能整除block_shape和block_unit
 constexpr size_t thread_shape = 8, block_dim = block_shape / thread_shape;
-// thread_unit应能整除block_unit，frag_size是中转寄存器最大尺寸
-constexpr size_t thread_unit = 1, frag_size = DIVUP(block_unit, block_dim);
+// thread_unit应能整除block_unit
+constexpr size_t thread_unit = 1;
 
 __global__ void kernel(const real (*A)[K], const real (*B)[N], real (*C)[N])
 {
@@ -19,9 +19,6 @@ __global__ void kernel(const real (*A)[K], const real (*B)[N], real (*C)[N])
 
     // 不是协同拷贝，每个线程拷贝自己所需的数据，不同线程会重复拷贝数据
     real r_a[2][thread_shape][thread_unit], r_b[2][thread_unit][thread_shape];
-
-    // 从全局内存转移到共享内存的中转寄存器
-    real frag_a[thread_shape][frag_size], frag_b[frag_size][thread_shape];
 
     real sum[thread_shape][thread_shape];
     for (size_t p = 0; p < thread_shape; ++p) {
@@ -70,18 +67,6 @@ __global__ void kernel(const real (*A)[K], const real (*B)[N], real (*C)[N])
             // 提前到最后一次小迭代之前，切换到下一批次共享内存
             if (j == block_unit / thread_unit) {
                 if (i != K / block_unit) {
-                    for (size_t p = 0; p < thread_shape; ++p) {
-                        for (size_t q = 0; tx + q * block_dim < block_unit; ++q) {
-                            s_a[smem_stage_idx ^ 1][ty + p * block_dim][tx + q * block_dim] =
-                                frag_a[p][q];
-                        }
-                    }
-                    for (size_t q = 0; ty + q * block_dim < block_unit; ++q) {
-                        for (size_t p = 0; p < thread_shape; ++p) {
-                            s_b[smem_stage_idx ^ 1][ty + q * block_dim][tx + p * block_dim] =
-                                frag_b[q][p];
-                        }
-                    }
                     // 避免在共享内存使用之前被修改
                     __syncthreads();
                 }
@@ -112,12 +97,14 @@ __global__ void kernel(const real (*A)[K], const real (*B)[N], real (*C)[N])
                     // 覆盖上一轮迭代计算使用的共享内存
                     for (size_t p = 0; p < thread_shape; ++p) {
                         for (size_t q = 0; tx + q * block_dim < block_unit; ++q) {
-                            frag_a[p][q] = A[iy + p * block_dim][col_a + q * block_dim];
+                            s_a[smem_stage_idx ^ 1][ty + p * block_dim][tx + q * block_dim] =
+                                A[iy + p * block_dim][col_a + q * block_dim];
                         }
                     }
                     for (size_t q = 0; ty + q * block_dim < block_unit; ++q) {
                         for (size_t p = 0; p < thread_shape; ++p) {
-                            frag_b[q][p] = B[row_b + q * block_dim][ix + p * block_dim];
+                            s_b[smem_stage_idx ^ 1][ty + q * block_dim][tx + p * block_dim]
+                                = B[row_b + q * block_dim][ix + p * block_dim];
                         }
                     }
                 }
